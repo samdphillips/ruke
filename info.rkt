7c6c7f543ec455ed5f5fbafac6ed213ea0517d6d
Sam Phillips <samdphillips@gmail.com>
2023-08-02 10:23:49 -0700

    Add basic info.rkt

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+
+(define name "ruke")
+(define collection "ruke")
+(define version "0.0.1")
+(define deps '("base" "rhombus-prototype"))
+(define build-deps '("base"))
+(define pkg-authors '(samdphillips@gmail.com))
+(define license 'Apache-2.0)
